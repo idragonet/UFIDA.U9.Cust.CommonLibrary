6d9bdac0081f316b895d6f0462b810a513c0758d
Long Zhang <pig1717@gmail.com>
2020-04-13 21:50:09 +0800

    通用帮助类:分析 url 字符串中的参数信息

---
+++ UFIDA.U9.Cust.FY.CommonLibrary/Helper/UFIDA.U9.Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UFIDA.U9.Cust.CommonLibrary.Helper
+{
+
+
+    /// <summary>
+    /// 通用帮助类:分析 url 字符串中的参数信息、Cookie操作
+    /// </summary>
+    public class Helper
+    {
+
+
+        /// <summary>
+        /// 分析 url 字符串中的参数信息
+        /// </summary>
+        /// <param name="url">输入的 URL</param>
+        /// <param name="baseUrl">输出 URL 的基础部分</param>
+        /// <param name="nvc">输出分析后得到的 (参数名,参数值) 的集合</param>
+        public static void ParseUrl(string url, out string baseUrl, out NameValueCollection nvc)
+        {
+            if (url == null)
+                throw new ArgumentNullException("url");
+            nvc = new NameValueCollection();
+            baseUrl = "";
+            if (url == "")
+                return;
+            int questionMarkIndex = url.IndexOf('?');
+            if (questionMarkIndex == -1)
+            {
+                baseUrl = url;
+                return;
+            }
+            baseUrl = url.Substring(0, questionMarkIndex);
+            if (questionMarkIndex == url.Length - 1)
+                return;
+            string ps = url.Substring(questionMarkIndex + 1);
+            // 开始分析参数对  
+            Regex re = new Regex(@"(^|&)?(\w+)=([^&]+)(&|$)?", RegexOptions.Compiled);
+            MatchCollection mc = re.Matches(ps);
+            foreach (Match m in mc)
+            {
+                nvc.Add(m.Result("$2").ToLower(), m.Result("$3"));
+            }
+        }
+
+
+    }
+}
